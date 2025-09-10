51d0338e65b568125e08b15c19be791c81a0aadd
Robert Ratajczak <r.ratajczak@tkhtechnology.com>
2025-09-10 17:14:08 +0200

    chore: MX002-13709 update packages

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ Package.describe({
   git: "https://github.com/tkhtechnology/layout.git",
   name: "cctech:layout",
   summary: "Polymer Layout renderer",
-  version: "1.1.24-dev.1"
+  version: "2.0.1-dev.1"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("1.6");
   api.use('ecmascript');
-  api.use('cctech:synthesis');
+  api.use('cctech:synthesis@1.3.17-dev.1');
 
   api.addFiles("PolymerLayout.js", ["client"]);
   api.addFiles("mwc_layout.html", ["client"]);
